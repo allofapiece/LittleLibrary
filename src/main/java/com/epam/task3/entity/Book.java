ac5773d9ff4048dffa13e4d0e23b5d0ba937186c
allofapiece <stasana1998@tut.by>
2018-06-17 00:37:35 +0300

    Created entities of subject area

---
+++ src/main/java/com/epam/task3/entity/Book.java
@@ -0,0 +1,93 @@
+package com.epam.task3.entity;
+
+/**
+ * The book entity is a part of subject area.
+ *
+ * @author Listratsenka Stanislau
+ * @version 1.0
+ */
+public class Book {
+    private int id;
+    private String ISDN;
+    private Status status;
+    private String title;
+    private String author;
+
+    /**
+     * Default constructor
+     */
+    public Book() {
+    }
+
+    public Book(int id, String ISDN, Status status, String title, String author) {
+        this.id = id;
+        this.ISDN = ISDN;
+        this.status = status;
+        this.title = title;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getISDN() {
+        return ISDN;
+    }
+
+    public void setISDN(String ISDN) {
+        this.ISDN = ISDN;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (id != book.id) return false;
+        if (ISDN != null ? !ISDN.equals(book.ISDN) : book.ISDN != null) return false;
+        if (status != book.status) return false;
+        if (title != null ? !title.equals(book.title) : book.title != null) return false;
+        return author != null ? author.equals(book.author) : book.author == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (ISDN != null ? ISDN.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/com/epam/task3/entity/Status.java
@@ -0,0 +1,32 @@
+package com.epam.task3.entity;
+
+/**
+ * Status class is a property of {@link Book book entity}.
+ * Values of this enumeration describes the
+ * current state of the book.
+ *
+ * @author Listratsenka Stanislau
+ * @version 1.0
+ */
+public enum Status {
+
+    /**
+     * This book is a available at this
+     * moment and can be taken by the visitors.
+     */
+    AVAILABLE,
+
+    /**
+     * This book on hands now and
+     * can't be used by other visitors in current
+     * reading session, if book owner wont transfer
+     * this book to other visitor.
+     */
+    ON_HANDS,
+
+    /**
+     * This book was taken for
+     * reading in the reading room of library
+     */
+    IN_READING_ROOM,
+}
